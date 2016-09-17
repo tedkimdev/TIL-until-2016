9e5c3669aeef2b48ed80daa59d3351f4396b6ffb
김태동 <yenafirst91@gmail.com>
2016-09-17 16:12:14 +0900

    [react] jsx, babel, webpack

--- react/v01.simplest/src/index.js
+++ react/v01.simplest/src/index.js
@@ -7,15 +7,20 @@ import ReactDOM from 'react-dom'
 class Examples extends Component {
   func01() {
   	console.log("Hello from func01");
-  	return "function01"
+  	return "function of class"
   }
   render() {
+  	const func02 = function(){
+  	  console.log("Hello from func02");
+  	  return "function in render()"
+    }
     return (
       <div>
         <div>Simple react</div>
         <div style={{color:"red"}}>Hello, World</div>
         <div> { 1+1 } </div>
         <div> {this.func01()} </div>
+        <div> {func02()} </div>
       </div>
     )
   }
