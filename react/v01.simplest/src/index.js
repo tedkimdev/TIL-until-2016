4f5e18105e4c7eaf99ee0047c2eae9dd4bd4b116
김태동 <yenafirst91@gmail.com>
2016-09-17 15:37:34 +0900

    [react] simplest react

---
+++ react/v01.simplest/src/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+
+class Examples extends Component {
+  render() {
+    return (
+      <div>Simple react</div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Examples/>,
+  document.getElementById('root')
+)
