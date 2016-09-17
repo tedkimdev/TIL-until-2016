7d5beee0e53911a72b4ce47aa809dbaee7835274
김태동 <yenafirst91@gmail.com>
2016-09-17 16:01:49 +0900

    [react] react & jsx

--- react/v01.simplest/src/index.js
+++ react/v01.simplest/src/index.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
+//1st option
+//jsx 문법을 따른다.
 
 class Examples extends Component {
+  func01() {
+  	console.log("Hello from func01");
+  	return "function01"
+  }
   render() {
     return (
-      <div>Simple react</div>
+      <div>
+        <div>Simple react</div>
+        <div style={{color:"red"}}>Hello, World</div>
+        <div> { 1+1 } </div>
+        <div> {this.func01()} </div>
+      </div>
     )
   }
 }
+
+//2sd option
+// class Examples extends Component {
+// }
+// Examples.prototype.render = function() {
+//     return (
+//       <div>Simple react</div>
+//     )
+// }
+
+//3rd option
+// function Examples(){}
+// Examples.prototype.render = function() {
+//     return (
+//       <div>Simple react</div>
+//     )
+// }
 
 ReactDOM.render(
   <Examples/>,
