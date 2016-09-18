5419476889222b4e68513ae5330420d312ee1d01
김태동 <yenafirst91@gmail.com>
2016-09-18 09:52:50 +0900

    [react] state

--- react/v02.State; React DevTools/src/index.js
+++ react/v02.State; React DevTools/src/index.js
@@ -8,17 +8,24 @@ class Examples extends Component {
   constructor(props) {
     super(props)
     this.state = {count: 0, sum: 3, a: 1, b: 2}
+    this.decrease = this.decrease.bind(this);
+  }
+  decrease() {
+    this.setState({count: this.state.count -1});
+    // this.state.count = this.state.count - 1;
+    // this.render();
   }
   render() {
+    console.log('Examples.render()');
     return (
       <div>
         <h1>React</h1>
         Clicked: <span>{this.state.count}</span> times
-        <button>Decrease</button>
+        <button onClick={this.decrease}>Decrease</button>
         <p/>
-        <input value={this.state.a}/> + <input value={this.state.b}/>
+        <input value={this.state.a} onChange={(e)=>this.setState({a: e.target.value})}/> + <input value={this.state.b} onChange={(e)=>this.setState({b: e.target.value})}/>
         = <span>{this.state.sum}</span>
-        <button>Sum</button>
+        <button onClick={() => this.setState({sum: Number(this.state.a) + Number(this.state.b)})}>Sum</button>
         <p/>
       </div>
     )
